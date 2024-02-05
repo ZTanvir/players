d38db28972a232dafe92e43214e15170a74506c3
ztanvir <zahirulopel@gmail.com>
2024-02-05 13:07:04 +0600

    Database mongodb connected to the app

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to mongodb");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to mongodb", error.message);
+  });
 
 app.get("/", (req, res) => {
   res.end("<h1>Hello players<h1>");
