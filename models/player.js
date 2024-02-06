011441b38b4a0b6d6051d009c920fcc0bdca53e7
ztanvir <zahirulopel@gmail.com>
2024-02-06 12:19:07 +0600

    Add player model with name,league,description must be required

---
+++ models/player.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const playerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  league: { type: String, required: true },
+  Description: { type: String, required: true },
+  Like: Number,
+  Dislike: Number,
+});
+
+playerSchema.set("toJSON", {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString();
+    delete returnObject._id;
+    delete returnObject.__v;
+  },
+});
+
+const Player = mongoose.model("Player", playerSchema);
+
+module.exports = Player;
