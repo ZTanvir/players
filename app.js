8911ca7237a4a63edaf909b924c15933b6f4ee49
ztanvir <zahirulopel@gmail.com>
2024-02-07 11:27:49 +0600

    Move database connection to player model

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const playerRoute = require("./controllers/players");
 
-mongoose.set("strictQuery", false);
-
-logger.info("connecting to", config.MONGODB_URI);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("Connected to mongodb");
-  })
-  .catch((error) => {
-    logger.error("Error connecting to mongodb", error.message);
-  });
 app.use(express.json());
 app.use("/api/player", playerRoute);
 
--- models/player.js
+++ models/player.js
@@ -1,4 +1,19 @@
 const mongoose = require("mongoose");
+const logger = require("../utils/logger");
+const config = require("../utils/config");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to mongodb");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to mongodb", error.message);
+  });
 
 const playerSchema = new mongoose.Schema({
   name: { type: String, required: true },
