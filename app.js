c7590061b5e3c8ca040ba3983191d700eea54144
ztanvir <zahirulopel@gmail.com>
2024-02-08 11:24:22 +0600

    Add middleware to tackle unKnownendPoint,errorHandler

--- app.js
+++ app.js
@@ -3,25 +3,18 @@ const app = express();
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const playerRoute = require("./controllers/players");
 
-mongoose.set("strictQuery", false);
-
-logger.info("connecting to", config.MONGODB_URI);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("Connected to mongodb");
-  })
-  .catch((error) => {
-    logger.error("Error connecting to mongodb", error.message);
-  });
 app.use(express.json());
+
 app.use("/api/player", playerRoute);
 
 app.get("/", (req, res) => {
   res.end("<h1>Hello players<h1>");
 });
+
+app.use(middleware.unknownEndPoint);
+app.use(middleware.errorHandlerFunction);
 
 module.exports = app;
